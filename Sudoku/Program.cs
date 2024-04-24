acf2768f4ea600a1fb5f41c887fba21b9b6b8029
Elfre Valdes <elfre.valdes@gmail.com>
2024-04-24 00:05:59 -0600

    Implements coordinates correctly according to game rules

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Drawing;
 
-const short Row = 0;
-const short uRow = 1;
-const short Column = 2;
-const short uColumn = 3;
+const short Column = 0;
+const short Row = 1;
+const short Value = 3;
+const char invalidCol = 'X';
+const short invalidRow = 0;
+const short invalidValue = 0;
+
 
 
 void gameIntro()
@@ -11,7 +15,7 @@ void gameIntro()
     Console.WriteLine("Welcome to Sudoku:");
     Console.WriteLine("The rules are simple select a column by saying c or C and a number follow by");
     Console.WriteLine("the row r or R follow by a number and then the number desired in that spot ");
-    Console.WriteLine("Example: c1r1 9 or C1R1 9");
+    Console.WriteLine("Example: c1 9 or C1 9");
     Console.WriteLine("If you want to quit the game just type quit or q and enter");
 }
 
@@ -21,7 +25,7 @@ void tString(string t)
 }
 bool isValidCoordinate(Coordinates coordenadas)
 {
-    if(coordenadas.Column == 0 && coordenadas.Row == 0 && coordenadas.Value == 0)
+    if(coordenadas.Column == invalidCol || coordenadas.Row == invalidRow || coordenadas.Value == invalidValue)
     {
         return false;
     }
@@ -30,28 +34,31 @@ bool isValidCoordinate(Coordinates coordenadas)
 
 Coordinates ParseCoordinate(string coordinate)
 {
-    if (coordinate.Length >= 5)
+    tString(coordinate.Length.ToString());
+    if (coordinate.Length != 4)
     {
         Console.WriteLine(coordinate + " is not a valid input");
-        return new Coordinates() { Column = 0, Row = 0, Value = 0 };
+        return new Coordinates() { Column = invalidCol, Row = invalidRow, Value = invalidValue };
     }
 
     string newCoor = coordinate.ToUpper();
-    tString(newCoor);
-    tString(newCoor[0].ToString());
-    tString(newCoor[1].ToString());
-    // Check if letters are correct
-    if (newCoor[0] == 'C' && newCoor[1] == 'R')
-    {
-        short row;
-        short column;
+    short row = 0;
+    short value = 0;
 
-        if (short.TryParse(newCoor.Substring(2, 2), out column) && short.TryParse(newCoor.Substring(4, 2), out row))
+
+    // Check if letters are correct
+    if (newCoor[Column] >= 'A' && newCoor[Column] <= 'I')
+    {
+        if (short.TryParse(newCoor[Row].ToString(), out row) && row >= 1 && row <= 9)
         {
-            short value;
-            if (short.TryParse(newCoor.Substring(6), out value))
+            if (short.TryParse(newCoor[Value].ToString(), out value) && value >= 1 && value <= 9)
             {
-                return new Coordinates() { Column = column, Row = row, Value = value };
+                return new Coordinates
+                {
+                    Column = newCoor[Column],
+                    Row = row,
+                    Value = value,
+                };
             }
             else
             {
@@ -60,15 +67,16 @@ Coordinates ParseCoordinate(string coordinate)
         }
         else
         {
-            Console.WriteLine(coordinate + " has an invalid column or row");
+            Console.WriteLine(coordinate + " has an invalid row");
         }
     }
     else
     {
-        Console.WriteLine(coordinate + " has an invalid format");
+        Console.WriteLine(coordinate + " has an invalid column");
     }
+   
 
-    return new Coordinates() { Column = 0, Row = 0, Value = 0 };
+    return new Coordinates() { Column = invalidCol, Row = invalidRow, Value = invalidValue };
 }
 
 void menu()
@@ -113,7 +121,7 @@ main();
 
 public struct Coordinates
 {
-    public short Column { get; set; }
+    public char Column { get; set; }
     public short Row { get; set; }
     public short Value { get; set; }
 }
