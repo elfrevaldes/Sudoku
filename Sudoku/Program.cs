3da5c77360564d498397449735754c0b3ca6262e
Elfre Valdes <elfre.valdes@gmail.com>
2024-04-24 10:23:38 -0600

    Implements: read board, make copy of board,  debug display, and regular display

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -7,8 +7,9 @@ const short Value = 3;
 const char invalidCol = 'X';
 const short invalidRow = 0;
 const short invalidValue = 0;
-
-
+const short NumRowCol = 9;
+short[,] board = new short[NumRowCol, NumRowCol];
+short[,] oriBoard = new short[NumRowCol, NumRowCol];
 
 void gameIntro()
 {
@@ -19,10 +20,116 @@ void gameIntro()
     Console.WriteLine("If you want to quit the game just type quit or q and enter");
 }
 
+bool readBoard(string fileName)
+{
+    /* debugging
+    string currentDirectory = Directory.GetCurrentDirectory();
+    Console.WriteLine($"Current Directory: {currentDirectory}");
+    
+    string[] filesInDirectory = Directory.GetFiles(currentDirectory);
+    Console.WriteLine("Files in current directory:");
+    foreach (string file in filesInDirectory)
+    {
+        Console.WriteLine(file);
+    }
+    end debug */
+
+    // Check if the file exists
+    if (File.Exists(fileName))
+    {
+        try
+        {
+            // Open the file and create a StreamReader to read from it
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                // Read each line of the file
+                for (int i = 0; i < NumRowCol; i++)
+                {
+                    string line = sr.ReadLine();
+                    if (line != null)
+                    {
+                        // Split the line into individual numbers
+                        string[] numbers = line.Split(' ');
+
+                        // Parse and populate the board array
+                        for (int j = 0; j < NumRowCol; j++)
+                        {
+                            board[i, j] = short.Parse(numbers[j]);
+                        }
+                    }
+                }
+                copyBoard(board); // Keep a copy for undo
+                return true; // file was read correctly
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error occurred while reading the file:");
+            Console.WriteLine(e.Message);
+        }
+    }
+    else
+    {
+        Console.WriteLine($"File '{fileName}' does not exist.");
+    }
+    return false; // we could not read the file
+}
+
+void displayBoardDebug(short[,] boardToDisplay)
+{
+    Console.WriteLine("Board Contents:");
+    for (int i = 0; i < NumRowCol; i++)
+    {
+        for (int j = 0; j < NumRowCol; j++)
+        {
+            Console.Write(boardToDisplay[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void copyBoard(short[,] boardToCopy)
+{
+    for (int i = 0; i < NumRowCol; i++)
+    {
+        for (int j = 0; j < NumRowCol; j++)
+        {
+            oriBoard[i,j] = boardToCopy[i, j];
+        }
+    }
+}
+
+void displayBoard()
+{
+    Console.WriteLine("   A B C D E F G H I");
+    for (int row = 0; row < NumRowCol; row++)
+    {
+        Console.Write(row + 1 + "| ");
+        for (int col = 0; col < NumRowCol; col++)
+        {
+            // do not display 0
+            if (board[row, col] == 0)
+            {
+                Console.Write("  ");
+            }
+            else
+            {
+                if (oriBoard[row, col] != 0)
+                { Console.ForegroundColor = ConsoleColor.Blue; }
+                Console.Write(board[row, col] + " ");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
 void tString(string t)
 {
     Console.WriteLine("Value: " + t);
 }
+
 bool isValidCoordinate(Coordinates coordinates)
 {
     if(coordinates.Column == invalidCol || coordinates.Row == invalidRow || coordinates.Value == invalidValue)
@@ -113,8 +220,14 @@ void menu()
     } while (!quit); 
 }
 
+
 void main()
 {
+    // \Sudoku\Sudoku\bin\Debug\net8.0\here is where the program is running
+    readBoard("..\\..\\..\\board.txt");
+    board[0, 0] = (short)1;
+    // displayBoardDebug(oriBoard);
+    displayBoard();
     gameIntro();
     menu();
 }
